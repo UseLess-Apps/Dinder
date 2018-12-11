f9cbc79e7268dad4b0b35d26b533bfd7832e4b2c
hackermanone <Ricky.chen98@hotmail.com>
2018-12-11 16:05:44 -0500

    home screen ui

---
+++ styles/Styles.js
@@ -0,0 +1,39 @@
+import { StyleSheet, Platform } from 'react-native';
+import Dimensions from 'Dimensions';
+
+var {height, width} = Dimensions.get('window');
+
+export const Styles = StyleSheet.create({
+  homeText: {
+    textAlign: 'center',
+    backgroundColor: 'blue',
+    marginTop: height * 0.1,
+    color: 'white',
+    fontSize: 50,
+  },
+  homeScreenContainer: {
+    backgroundColor: 'red',
+    height: height,
+    flexDirection : 'column',
+  },
+  location: {
+    height: height * 0.1,
+    padding: 10,
+    flex: 0.8,
+    borderRadius: 5,
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+  locationImage: {
+    height: height * 0.1,
+    flex: 0.2,
+  },
+  flexRow: {
+    flexDirection: 'row',
+    marginTop: height * 0.2,
+    marginLeft: width * 0.05,
+    marginRight: width * 0.05,
+    height: height * 0.1,
+  },
+
+  });
